e004f98e56de62c0e87efa321a67fc9e41dcdef9
Here East <nastiapiven@gmail.com>
2023-10-29 18:45:32 +0100

    feat: create App component
    (cherry picked from commit 8c95c49df047f7d5fd3006bb7e2e8a55e6debcf7)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./components/App.tsx";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
