c6732dc677f27448e3d9e6871757b2e9c93502f4
Here East <nastiapiven@gmail.com>
2023-11-01 22:15:27 +0100

    feat: create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,27 @@
+import { ReactElement, MouseEvent } from "react";
+
+import classnames from "classnames";
+import styles from "./button.module.scss";
+
+interface ButtonProps {
+  type: "submit" | "reset" | "button";
+  title: string;
+  className?: string;
+  disabled?: boolean;
+  onClick: (event?: MouseEvent<HTMLButtonElement>) => void;
+}
+
+function Button({ title, type, className, disabled, onClick }: ButtonProps): ReactElement {
+  return (
+    <button
+      type={type}
+      className={classnames(styles.button, className || "")}
+      onClick={(event): void => onClick(event)}
+      disabled={disabled}
+    >
+      {title}
+    </button>
+  );
+}
+
+export default Button;
