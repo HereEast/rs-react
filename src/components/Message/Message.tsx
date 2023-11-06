bb99c39b511921d75b473be7d929d1234ba47a0d
Here East <nastiapiven@gmail.com>
2023-11-06 13:49:33 +0100

    refactor: refactor Message component

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,16 +1,19 @@
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
 
+import classnames from "classnames";
 import styles from "./message.module.scss";
 
 interface MessageProps {
   message: string;
   className?: string;
+  children?: ReactNode;
 }
 
-function Message({ message, className }: MessageProps): ReactElement {
+function Message({ message, className, children }: MessageProps): ReactElement {
   return (
-    <div className={styles.message}>
-      <span className={className}>{message}</span>
+    <div className={classnames(styles.message, className)}>
+      <span>{message}</span>
+      {children}
     </div>
   );
 }
