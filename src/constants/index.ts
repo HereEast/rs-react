263adb6b32def4d7ac8f7836279c49e95fc1b449
Here East <nastiapiven@gmail.com>
2023-11-07 17:53:29 +0100

    feat: update constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 export const MAX_COUNT = "300";
 export const LIMIT = "30";
-export const PAGE_ITEMS = [30, 50, 60, 100];
 export const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+export const INIT_PARAMS = `limit=${LIMIT}&offset=1`;
+export const RANGE_OPTIONS = [10, 20, 30, 40, 50, 80, 100];
