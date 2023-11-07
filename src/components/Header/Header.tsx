bb507958b80b8921d28bb6200eb62635d8222504
Here East <nastiapiven@gmail.com>
2023-11-07 17:49:28 +0100

    fix: update Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from "react";
 import { SearchInput } from "../SearchInput";
 import { Button } from "../Button";
 
+import styles from "./header.module.scss";
+
 interface HeaderProps {
   isLoading: boolean;
   onSearch: () => void;
@@ -10,8 +12,10 @@ interface HeaderProps {
 
 function Header({ isLoading, onSearch, throwError }: HeaderProps): ReactElement {
   return (
-    <header className="header">
-      <span className="note">The total number of Pokémon is limited to 300 due to practical considerations.</span>
+    <header className={styles.header}>
+      <span className={styles.note}>
+        The total number of Pokémon is limited to 300 due to practical considerations.
+      </span>
 
       <SearchInput onSearch={onSearch} isLoading={isLoading} />
       <Button name="Throw Error ⚡️" onClick={throwError} disabled={isLoading} />
