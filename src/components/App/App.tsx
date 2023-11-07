d06d588c05e3c76c7793ab9ff1e327b7ea0e8c43
Here East <nastiapiven@gmail.com>
2023-11-07 17:57:27 +0100

    feat: add Details context to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,19 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "../../router";
 import { ErrorBoundary } from "../ErrorBoundary";
+import { DetailsContext } from "../../context";
 
 function App(): ReactElement {
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
   return (
     <ErrorBoundary>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <DetailsContext.Provider value={{ selectedItem, setSelectedItem }}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </DetailsContext.Provider>
     </ErrorBoundary>
   );
 }
