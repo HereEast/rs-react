883e6f407956517646bec7deb280471f8f56b60d
Here East <nastiapiven@gmail.com>
2023-11-08 14:56:54 +0100

    fix: fix NotFound link path

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
 import { Message } from "../../components/Message";
+import { INIT_PARAMS } from "../../constants";
 
 import classnames from "classnames";
 import styles from "./notFound.module.scss";
@@ -8,7 +9,7 @@ import styles from "./notFound.module.scss";
 function NotFound(): ReactElement {
   return (
     <Message message="Page not found (404)">
-      <Link to="/" className={classnames(styles.link, styles.link_button)}>
+      <Link to={`/?${INIT_PARAMS}`} className={classnames(styles.link, styles.link_button)}>
         Back to main
       </Link>
     </Message>
