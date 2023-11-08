bd0cf19ede897b3d1769ec2a7ac2964b38865377
Here East <nastiapiven@gmail.com>
2023-11-08 14:54:09 +0100

    refactor: refactor SearchResults component

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -2,28 +2,28 @@ import { ReactElement } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Card } from "../Card";
 import { IPokemonData } from "../../types/types";
-import { LIMIT, MAX_COUNT } from "../../constants";
+import { LIMIT, MAX_COUNT, MIN_COUNT } from "../../constants";
 
 import styles from "./searchResults.module.scss";
 
 interface SearchResultsProps {
   searchResults: IPokemonData[];
-  page?: string;
-  // limit: string;
 }
 
-function SearchResults({ searchResults, page }: SearchResultsProps): ReactElement {
+function SearchResults({ searchResults }: SearchResultsProps): ReactElement {
   const [searchParams] = useSearchParams();
+
   const limit = searchParams.get("limit") || LIMIT;
+  const page = searchParams.get("offset") || MIN_COUNT;
 
   const lastPage = Math.ceil(Number(MAX_COUNT) / Number(limit));
   const lastPageCount = Number(MAX_COUNT) - Number(limit) * (lastPage - 1);
 
-  const array = Number(page) === lastPage ? [...searchResults].slice(0, lastPageCount) : searchResults;
+  const pageResults = Number(page) === lastPage ? [...searchResults].slice(0, lastPageCount) : searchResults;
 
   return (
     <div className={styles.results}>
-      {array.length > 0 && array.map((data) => <Card key={data.id} name={data.name} image={data.image} />)}
+      {pageResults.length > 0 && pageResults.map((data) => <Card key={data.id} name={data.name} image={data.image} />)}
     </div>
   );
 }
