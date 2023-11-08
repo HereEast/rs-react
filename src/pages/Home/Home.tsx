f9ef45c05e9bb583b0c47f80d7ef5d6bc55e4ffc
Here East <nastiapiven@gmail.com>
2023-11-08 19:37:25 +0100

    feat: add NotFound to Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import { ReactElement, useEffect, useState } from "react";
-import { useSearchParams, Outlet } from "react-router-dom";
+import { useSearchParams, Outlet, useLocation } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Message } from "../../components/Message";
 import { SearchResults } from "../../components/SearchResults";
 import { Pagination } from "../../components/Pagination";
-import { useFetchPokemon, useDetailsContext } from "../../hooks";
-import { INIT_PARAMS } from "../../constants";
+import { NotFound } from "../NotFound";
+import { useFetchPokemon, useDetailsContext, useMaxPage } from "../../hooks";
+import { INIT_PARAMS, LIMIT, MIN_COUNT } from "../../constants";
 
 import classnames from "classnames";
 import styles from "./home.module.scss";
@@ -13,17 +14,33 @@ import styles from "./home.module.scss";
 // Limit page and limit
 
 function Home(): ReactElement {
+  const location = useLocation();
+
   const { selectedItem } = useDetailsContext();
+
+  const { maxPage, getMaxPage } = useMaxPage();
   const { getPokemon, getAllPokemon, searchResults, isLoading, error } = useFetchPokemon();
 
   const [searchParams, setSearchParams] = useSearchParams(INIT_PARAMS);
   const [isError, setIsError] = useState(false);
+  const [notFound, setNotFound] = useState(false);
 
   useEffect(() => {
-    setSearchParams(searchParams.toString());
+    setNotFound(false);
 
-    const savedSearchString = localStorage.getItem("searchString") || "";
-    handleSearch(savedSearchString);
+    if (location.pathname !== "/") {
+      setNotFound(true);
+    } else {
+      getMaxPage(searchParams);
+
+      const limit = parseInt(searchParams.get("limit") || LIMIT, 10);
+      const page = parseInt(searchParams.get("offset") || MIN_COUNT, 10);
+
+      setSearchParams({ limit: String(limit), offset: String(page) });
+    }
+
+    const searchString = localStorage.getItem("searchString") || "";
+    handleSearch(searchString);
   }, [searchParams]);
 
   useEffect(() => {
@@ -45,24 +62,29 @@ function Home(): ReactElement {
   }
 
   return (
-    <div className={classnames(styles.page, selectedItem ? styles.page__split : "")}>
-      <section className={classnames(styles.page__column, styles.page__results)}>
-        <Header isLoading={isLoading} onSearch={handleSearch} throwError={handleThrowError} />
+    <>
+      {notFound && <NotFound />}
+      {!notFound && (
+        <div className={classnames(styles.page, selectedItem ? styles.page__split : "")}>
+          <section className={classnames(styles.page__column, styles.page__results)}>
+            <Header isLoading={isLoading} onSearch={handleSearch} throwError={handleThrowError} />
 
-        <Pagination isLoading={isLoading} />
+            <Pagination isLoading={isLoading} maxPage={maxPage} />
 
-        {error && <Message message={error} />}
-        {isLoading && <Message message="Loading..." />}
+            {error && <Message message={error} />}
+            {isLoading && <Message message="Loading..." />}
 
-        {!error && !isLoading && <SearchResults searchResults={searchResults} />}
-      </section>
+            {!error && !isLoading && <SearchResults searchResults={searchResults} />}
+          </section>
 
-      {selectedItem && (
-        <section className={classnames(styles.page__column, styles.page__details)}>
-          <Outlet />
-        </section>
+          {selectedItem && (
+            <section className={classnames(styles.page__column, styles.page__details)}>
+              <Outlet />
+            </section>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
