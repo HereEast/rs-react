894bff99493f9d0ea55ea0fbeefd35c093bf3cfa
Here East <nastiapiven@gmail.com>
2023-11-12 17:06:24 +0100

    feat: add test for NotFound

---
+++ src/pages/NotFound/NotFound.test.tsx
@@ -0,0 +1,21 @@
+import "@testing-library/jest-dom";
+
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { NotFound } from "./index";
+
+describe("NotFound page component", () => {
+  test("should render aa message and a button", () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const message = screen.getByText("Page not found (404)");
+    const button = screen.getByRole("button", { name: "Back to main" });
+
+    expect(message).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+});
