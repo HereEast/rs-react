fb7b4a9fdee84ebfdaff5c71e4e2ba9bfe7b158b
Here East <nastiapiven@gmail.com>
2023-11-12 21:11:13 +0100

    feat: add test for NotFound checking invalid path

--- src/pages/NotFound/NotFound.test.tsx
+++ src/pages/NotFound/NotFound.test.tsx
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom";
 
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { NotFound } from "./index";
 
 describe("NotFound page component", () => {
@@ -12,10 +13,24 @@ describe("NotFound page component", () => {
       </MemoryRouter>,
     );
 
-    const message = screen.getByText("Page not found (404)");
+    const message = screen.getByText(/Page not found/i);
     const button = screen.getByRole("button", { name: /Back to main/i });
 
     expect(message).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+
+  test("displays NotFound page for invalid route", () => {
+    render(
+      <MemoryRouter initialEntries={["/not-found"]}>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    const message = screen.getByText(/Page not found/i);
+    expect(message).toBeInTheDocument();
   });
 });
