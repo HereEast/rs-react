c7ef60bfbe69ccfd4a942c2bda44491e1ca358cc
Here East <nastiapiven@gmail.com>
2023-11-14 19:28:36 +0100

    feat: add tests

--- src/components/Details/Details.test.tsx
+++ src/components/Details/Details.test.tsx
@@ -1,8 +1,10 @@
 import "@testing-library/jest-dom";
 
-import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import { render, screen, within, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { Details } from "./index";
+import { mockPokemonData } from "../../__mocks__/pokemonData";
 import * as useFetchPokemon from "../../hooks/useFetchPokemon";
 
 const mockSetSelectedItem = jest.fn();
@@ -26,8 +28,68 @@ jest.mock("react-router-dom", () => ({
 }));
 
 const useFetchPokemonMock = jest.spyOn(useFetchPokemon, "useFetchPokemon");
+const getPokemonMock = jest.fn();
 
 describe("Details component", () => {
+  test("should correctly display the detailed card data", async () => {
+    getPokemonMock.mockResolvedValue([mockPokemonData]);
+    useFetchPokemonMock.mockReturnValue({
+      getPokemon: getPokemonMock,
+      getAllPokemon: jest.fn(),
+      isLoading: false,
+      error: "",
+    });
+
+    render(
+      <MemoryRouter>
+        <Details />
+      </MemoryRouter>,
+    );
+
+    await waitFor(() => {
+      expect(getPokemonMock).toHaveBeenCalled();
+    });
+
+    const panel = screen.getByTestId("details");
+    expect(panel).toBeInTheDocument();
+
+    const heading = await within(panel).findByRole("heading", { name: /pikachu/i });
+    expect(heading).toBeInTheDocument();
+
+    screen.debug();
+  });
+
+  test("should hide component clicking on Close button", async () => {
+    getPokemonMock.mockResolvedValue([mockPokemonData]);
+    useFetchPokemonMock.mockReturnValue({
+      getPokemon: getPokemonMock,
+      getAllPokemon: jest.fn(),
+      isLoading: false,
+      error: "",
+    });
+
+    render(
+      <MemoryRouter>
+        <Details />
+      </MemoryRouter>,
+    );
+
+    await waitFor(() => {
+      expect(getPokemonMock).toHaveBeenCalled();
+    });
+
+    const panel = screen.getByTestId("details");
+    expect(panel).toBeInTheDocument();
+
+    const button = await within(panel).findByRole("button", { name: /close/i });
+    expect(button).toBeInTheDocument();
+
+    await user.click(button);
+
+    expect(mockSetSelectedItem).toHaveBeenCalledWith(null);
+    expect(mockedUsedNavigate).toHaveBeenCalledWith("/?limit=30&page=1");
+  });
+
   test("should show loader when data is fetching", async () => {
     useFetchPokemonMock.mockReturnValue({
       getPokemon: jest.fn(),
@@ -62,29 +124,5 @@ describe("Details component", () => {
 
     const error = screen.getByText(/Oops!.. Something wrong. Try again!/i);
     expect(error).toBeInTheDocument();
-  });
-
-  test("should correctly display the detailed card data", async () => {
-    useFetchPokemonMock.mockReturnValue({
-      getPokemon: jest.fn(),
-      getAllPokemon: jest.fn(),
-      isLoading: false,
-      error: "",
-    });
-
-    render(
-      <MemoryRouter>
-        <Details />
-      </MemoryRouter>,
-    );
-
-    const panel = screen.getByTestId("details");
-    expect(panel).toBeInTheDocument();
-
-    // const heading = await within(panel).findByRole("heading", { name: /pikachu/i });
-    // const button = within(panel).getByRole("button", { name: /close/i });
-
-    // expect(button).toBeInTheDocument();
-    // expect(heading).toBeInTheDocument();
   });
 });
