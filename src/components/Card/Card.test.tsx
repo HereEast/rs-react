e3568b7963a5e6a8672aee463c7adaa345369bc1
Here East <nastiapiven@gmail.com>
2023-11-17 11:27:54 +0100

    refactor: remove comments

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -5,35 +5,6 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { Card } from "./index";
 
-// interface ISearchResults {
-//   searchResults: { name: string; image: string }[];
-// }
-
-// jest.mock("../../hooks/useAppContext", () => ({
-//   useAppContext: (): ISearchResults => ({
-//     searchResults: [{ name: "Pokemon", image: "pokemon.jpg" }],
-//   }),
-// }));
-
-// describe("Card component", () => {
-//   test("should render the relevant card data", () => {
-//     render(
-//       <MemoryRouter>
-//         <Card name="Pokemon" image="pokemon.jpg" />
-//       </MemoryRouter>,
-//     );
-
-//     const card = screen.getByTestId("card");
-//     const cardTitle = screen.getByRole("heading", { name: /Pokemon/i });
-//     const cardImage = screen.getByRole("img", { name: /Pokemon/i });
-
-//     expect(card).toBeInTheDocument();
-//     expect(cardTitle).toBeInTheDocument();
-//     expect(cardImage).toBeInTheDocument();
-//     expect(cardImage).toHaveAttribute("src", "pokemon.jpg");
-//   });
-// });
-
 const mockSetSelectedItem = jest.fn();
 const mockedUsedNavigate = jest.fn();
 
