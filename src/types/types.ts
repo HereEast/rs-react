42107e96749e9174e6ff8a39a8dee5fc1bb977de
Here East <nastiapiven@gmail.com>
2023-11-17 11:00:17 +0100

    feat: add api response type

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,14 +6,49 @@ export interface IPokemonData {
   image: string;
 }
 
-export interface IPokemonBasicData {
-  name: string;
-  url: string;
-}
-
 export interface IInitialPokemonSlice {
   status: string;
   error: string;
   isLoading: boolean;
   searchResults: IPokemonData[];
+}
+
+export interface IPokemonBasic {
+  name: string;
+  url: string;
+}
+
+interface IPokemonSprites {
+  back_default: string | null;
+  back_female: string | null;
+  back_shiny: string | null;
+  back_shiny_female: string | null;
+  front_default: string | null;
+  front_female: string | null;
+  front_shiny: string | null;
+  front_shiny_female: string | null;
+  other: {
+    dream_world: {
+      front_default: string | null;
+      front_female: string | null;
+    };
+    home: {
+      front_default: string | null;
+      front_female: string | null;
+      front_shiny: string | null;
+      front_shiny_female: string | null;
+    };
+    "official-artwork": {
+      front_default: string | null;
+      front_shiny: string | null;
+    };
+  };
+}
+
+export interface IPokemonApiResponse {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  sprites: IPokemonSprites;
 }
