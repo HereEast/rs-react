b5618dd812c6bad5602009acef76d204ba8d9d75
Here East <nastiapiven@gmail.com>
2023-11-19 17:18:49 +0100

    feat: add mock to api response data

--- src/__mocks__/pokemonData.ts
+++ src/__mocks__/pokemonData.ts
@@ -1,7 +1,24 @@
-export const mockPokemonData = {
+import { IPokemonData, IPokemonApiResponse } from "../types/types";
+
+export const mockPokemonData: IPokemonData = {
   id: 1,
   name: "pikachu",
-  weight: 10,
-  height: 20,
+  weight: 20,
+  height: 40,
   image: "pikachu.png",
+};
+
+export const mockApiResponse: IPokemonApiResponse = {
+  id: 1,
+  name: "pikachu",
+  weight: 20,
+  height: 40,
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: "pikachu.png",
+        front_shiny: "",
+      },
+    },
+  },
 };
