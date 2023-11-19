e1f9a83da9f957630f82aacf330f02a1df92746e
Here East <nastiapiven@gmail.com>
2023-11-19 17:39:55 +0100

    feat: add test for getMaxPage function

--- src/__test__/getMaxPage.test.tsx
+++ src/__test__/getMaxPage.test.tsx
@@ -1,14 +1,23 @@
 import "@testing-library/jest-dom";
 
-// import { getMaxPage } from "../utils";
+import { getMaxPage } from "../utils";
+import { BASE_URL } from "../constants";
 
-// const mockLimitParams = new URLSearchParams("limit=10");
-// const mockData = { count: 50 };
-
-// const fetchMock = fetch as jest.Mock;
+const mockLimitParams = new URLSearchParams("limit=10");
+const mockData = { count: 50 };
 
 describe("getMaxPage function", () => {
   it("should return the correct maximum page", async () => {
-    expect(true).toBe(true);
+    globalThis.fetch = jest.fn(
+      () =>
+        Promise.resolve({
+          json: async () => mockData,
+        }) as Promise<Response>,
+    );
+
+    const maxPage = await getMaxPage(mockLimitParams);
+    expect(maxPage).toBe("5");
+
+    expect(globalThis.fetch).toHaveBeenCalledWith(BASE_URL);
   });
 });
