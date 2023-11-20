0052545502a2ef031bb1b459f71c3325a5fdde08
Here East <nastiapiven@gmail.com>
2023-11-20 18:35:04 +0100

    feat: add test bor Button comp

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,7 +1,10 @@
 import "@testing-library/jest-dom";
 
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import { Button } from "./index";
+
+const handleClick = jest.fn();
 
 describe("Button component", () => {
   test("should be rendered with correct props", () => {
@@ -18,5 +21,23 @@ describe("Button component", () => {
     expect(button).toBeInTheDocument();
     expect(button).toHaveClass("button__test");
     expect(button).toHaveAttribute("type", "button");
+  });
+
+  test("should call callback function if it's passed as a parameter", async () => {
+    const props = {
+      name: "Button",
+      className: "button__test",
+      type: "button",
+      onClick: (): void => handleClick(),
+    } as const;
+
+    render(<Button {...props} />);
+
+    const button = screen.getByRole("button", { name: /Button/i });
+    expect(button).toBeInTheDocument();
+
+    await userEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalled();
   });
 });
