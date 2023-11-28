bf495731abfcd34ec9fb7633637548c9f8d9c106
Here East <nastiapiven@gmail.com>
2023-11-28 22:55:28 +0100

    refactor: clean code

--- src/hooks/useAppContext.ts
+++ src/hooks/useAppContext.ts
@@ -1,14 +1,9 @@
 import { useContext, Dispatch } from "react";
 import { AppContext } from "../context";
-// import { IPokemonData } from "../types/types";
 
 interface IAppContext {
   selectedItem: string;
-  // searchString: string;
-  // searchResults: IPokemonData[] | undefined;
   setSelectedItem: Dispatch<React.SetStateAction<string>>;
-  // setSearchString: Dispatch<React.SetStateAction<string>>;
-  // setSearchResults: Dispatch<React.SetStateAction<IPokemonData[] | undefined>>;
 }
 
 export function useAppContext(): IAppContext {
