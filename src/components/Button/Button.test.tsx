15856aaad5aa4bb625152503f911ebe492b6a913
Here East <nastiapiven@gmail.com>
2023-11-29 21:12:40 +0100

    feat: update tests for Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,44 +1,43 @@
 import "@testing-library/jest-dom";
 
-// import userEvent from "@testing-library/user-event";
-// import { render, screen } from "@testing-library/react";
-// import { Button } from "./index";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { Button } from "./index";
 
-// const handleClick = jest.fn();
+const handleClick = jest.fn();
 
 describe("Button component", () => {
   test("should be rendered with correct props", () => {
-    expect(true).toBe(true);
-    // const props = {
-    //   name: "Button",
-    //   className: "button__test",
-    //   type: "button",
-    // } as const;
+    const props = {
+      name: "Button",
+      className: "button__test",
+      type: "button",
+    } as const;
 
-    // render(<Button {...props} />);
+    render(<Button {...props} />);
 
-    // const button = screen.getByRole("button", { name: /Button/i });
+    const button = screen.getByRole("button", { name: /Button/i });
 
-    // expect(button).toBeInTheDocument();
-    // expect(button).toHaveClass("button__test");
-    // expect(button).toHaveAttribute("type", "button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass("button__test");
+    expect(button).toHaveAttribute("type", "button");
   });
 
-  // test("should call callback function if it's passed as a parameter", async () => {
-  //   const props = {
-  //     name: "Button",
-  //     className: "button__test",
-  //     type: "button",
-  //     onClick: (): void => handleClick(),
-  //   } as const;
+  test("should call callback function if it's passed as a parameter", async () => {
+    const props = {
+      name: "Button",
+      className: "button__test",
+      type: "button",
+      onClick: (): void => handleClick(),
+    } as const;
 
-  //   render(<Button {...props} />);
+    render(<Button {...props} />);
 
-  //   const button = screen.getByRole("button", { name: /Button/i });
-  //   expect(button).toBeInTheDocument();
+    const button = screen.getByRole("button", { name: /Button/i });
+    expect(button).toBeInTheDocument();
 
-  //   await userEvent.click(button);
+    await userEvent.click(button);
 
-  //   expect(handleClick).toHaveBeenCalled();
-  // });
+    expect(handleClick).toHaveBeenCalled();
+  });
 });
