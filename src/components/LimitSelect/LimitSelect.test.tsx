ce9fa123d38f55ec1a7a4e225dba0c3d95f5426f
Here East <nastiapiven@gmail.com>
2023-11-30 00:35:24 +0100

    feat: update tests for LimitSelect

--- src/components/LimitSelect/LimitSelect.test.tsx
+++ src/components/LimitSelect/LimitSelect.test.tsx
@@ -1,77 +1,61 @@
 import "@testing-library/jest-dom";
 
-// import * as reduxHooks from "react-redux";
-// import user from "@testing-library/user-event";
-// import { render, screen } from "@testing-library/react";
-// import { MemoryRouter } from "react-router-dom";
-// import { LimitSelect } from "./index";
-// import { RANGE_OPTIONS } from "../../constants";
+import * as reduxHooks from "react-redux";
+import user from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { useRouter } from "next/router";
+import { LimitSelect } from "./index";
+import { RANGE_OPTIONS } from "../../constants";
 
-// jest.mock("react-redux");
+jest.mock("react-redux");
 
-// jest.spyOn(reduxHooks, "useDispatch").mockReturnValue(jest.fn());
-// jest.spyOn(reduxHooks, "useSelector").mockReturnValue({ isLoading: false });
+jest.spyOn(reduxHooks, "useDispatch").mockReturnValue(jest.fn());
+jest.spyOn(reduxHooks, "useSelector").mockReturnValue({ isLoading: false });
 
-// type Param = { page: string; limit: string };
-// type UseSearchParamsReturn = [URLSearchParams, (params: Param) => void];
-
-// const mockSetSearchParams = jest.spyOn(URLSearchParams.prototype, "set");
-// const mockSearchParams = new URLSearchParams("limit=10&page=5");
-
-// jest.mock("react-router-dom", () => ({
-//   ...jest.requireActual("react-router-dom"),
-//   useSearchParams: (): UseSearchParamsReturn => [
-//     mockSearchParams,
-//     (params: Param): void => {
-//       mockSearchParams.set("page", params.page);
-//       mockSearchParams.set("limit", params.limit);
-//     },
-//   ],
-// }));
-
-// function renderLimitSelect(): void {
-//   render(
-//     <MemoryRouter>
-//       <LimitSelect />
-//     </MemoryRouter>,
-//   );
-// }
+jest.mock("next/router", () => {
+  const router = {
+    push: jest.fn(),
+    query: {},
+  };
+  return {
+    useRouter: jest.fn().mockReturnValue(router),
+  };
+});
 
 describe("LimitSelect component", () => {
-  // beforeEach(() => {
-  //   jest.clearAllMocks();
-  // });
-
-  test("should check that component is rendered and has all options", async () => {
-    expect(true).toBe(true);
-    // renderLimitSelect();
-
-    // const selectElement = screen.getByRole("combobox");
-    // expect(selectElement).toBeInTheDocument();
-
-    // for (const option of RANGE_OPTIONS) {
-    //   expect(screen.getByText(option)).toBeInTheDocument();
-    // }
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  // test("should change current value on change event", async () => {
-  //   renderLimitSelect();
+  test("should check that component is rendered and has all options", async () => {
+    render(<LimitSelect />);
 
-  //   const selectElement = screen.getByRole("combobox");
+    const selectElement = screen.getByRole("combobox");
+    expect(selectElement).toBeInTheDocument();
 
-  //   await user.selectOptions(selectElement, "80");
-  //   expect(selectElement).toHaveValue("80");
-  // });
+    for (const option of RANGE_OPTIONS) {
+      expect(screen.getByText(option)).toBeInTheDocument();
+    }
+  });
 
-  // test("should update page to 1 on limit change", async () => {
-  //   renderLimitSelect();
+  test("should change current value on change event", async () => {
+    render(<LimitSelect />);
 
-  //   const selectElement = screen.getByRole("combobox");
+    const selectElement = screen.getByRole("combobox");
 
-  //   await user.selectOptions(selectElement, "80");
+    await user.selectOptions(selectElement, "80");
 
-  //   expect(mockSetSearchParams).toHaveBeenCalledTimes(2);
-  //   expect(mockSetSearchParams).toHaveBeenNthCalledWith(1, "page", "1");
-  //   expect(mockSetSearchParams).toHaveBeenNthCalledWith(2, "limit", "80");
-  // });
+    expect(selectElement).toHaveValue("80");
+  });
+
+  test("should update page to 1 and limit to selected limit on limit change", async () => {
+    render(<LimitSelect />);
+
+    const selectElement = screen.getByRole("combobox");
+
+    await user.selectOptions(selectElement, "80");
+
+    expect(useRouter().push).toHaveBeenCalledTimes(1);
+    expect(useRouter().push).toHaveBeenCalledWith({ pathname: "/", query: { limit: "80", page: "1" } });
+  });
 });
