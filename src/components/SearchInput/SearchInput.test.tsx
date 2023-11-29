08231216a91da92cee51ed0153d2279f50a4a9f7
Here East <nastiapiven@gmail.com>
2023-11-29 23:01:41 +0100

    feat: update tests for SearchInput

--- src/components/SearchInput/SearchInput.test.tsx
+++ src/components/SearchInput/SearchInput.test.tsx
@@ -1,124 +1,118 @@
 import "@testing-library/jest-dom";
 
-// import * as reduxHooks from "react-redux";
-// import * as actions from "../../store/search/slice";
-// import user from "@testing-library/user-event";
-// import { render, screen, waitFor } from "@testing-library/react";
-// import { MemoryRouter } from "react-router-dom";
-// import { SearchInput } from "./index";
-// import { AppContext } from "../../context";
-// import { contextMock } from "../../__mocks__/contextMock";
+import * as reduxHooks from "react-redux";
+import * as actions from "../../store/search/slice";
+import user from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
+import { SearchInput } from "./index";
 
-// jest.mock("react-redux");
+jest.mock("react-redux");
 
-// const mockedDispatch = jest.spyOn(reduxHooks, "useDispatch") as jest.Mock;
-// const mockedSaveString = jest.spyOn(actions, "saveSearchString") as jest.Mock;
+const mockedDispatch = jest.spyOn(reduxHooks, "useDispatch") as jest.Mock;
+const mockedSaveString = jest.spyOn(actions, "saveSearchString") as jest.Mock;
 
-// jest.spyOn(reduxHooks, "useSelector").mockReturnValue({ isLoading: false });
+jest.spyOn(reduxHooks, "useSelector").mockReturnValue({ isLoading: false });
 
-// const mockSetItem = jest.spyOn(Storage.prototype, "setItem");
-// const mockGetItem = jest.spyOn(Storage.prototype, "getItem");
+jest.mock("next/router", () => {
+  const router = {
+    push: jest.fn(),
+    query: { limit: "30", page: "1" },
+  };
+  return {
+    useRouter: jest.fn().mockReturnValue(router),
+  };
+});
 
-// function renderSearchInput(): void {
-//   render(
-//     <AppContext.Provider value={contextMock}>
-//       <MemoryRouter>
-//         <SearchInput />
-//       </MemoryRouter>
-//     </AppContext.Provider>,
-//   );
-// }
+const mockSetItem = jest.spyOn(Storage.prototype, "setItem");
+const mockGetItem = jest.spyOn(Storage.prototype, "getItem");
 
 describe("SearchInput component", () => {
-  // beforeEach(() => {
-  //   mockSetItem.mockClear();
-  //   mockGetItem.mockClear();
-  //   mockedDispatch.mockClear();
-  //   mockedSaveString.mockClear();
-  //   localStorage.clear();
-  // });
-
-  test("should call dispatch when there is no input value", async () => {
-    expect(true).toBe(true);
-    // const dispatch = jest.fn();
-    // mockedDispatch.mockReturnValue(dispatch);
-
-    // renderSearchInput();
-
-    // const inputElement: HTMLInputElement = screen.getByRole("textbox");
-    // expect(inputElement).toHaveValue("");
-
-    // const searchButton = screen.getByRole("button", { name: /search/i });
-    // await user.click(searchButton);
-
-    // await waitFor(() => {
-    //   expect(dispatch).toHaveBeenCalled();
-    // });
+  beforeEach(() => {
+    jest.clearAllMocks();
+    localStorage.clear();
   });
 
-  // test("should call dispatch with inputValue when there is input value", async () => {
-  //   const dispatch = jest.fn();
-  //   mockedDispatch.mockReturnValue(dispatch);
+  test("should call dispatch when there is no input value", async () => {
+    const dispatch = jest.fn();
+    mockedDispatch.mockReturnValue(dispatch);
 
-  //   renderSearchInput();
+    render(<SearchInput />);
 
-  //   const inputElement: HTMLInputElement = screen.getByRole("textbox");
-  //   await user.type(inputElement, "pikachu");
+    const inputElement: HTMLInputElement = screen.getByRole("textbox");
+    expect(inputElement).toHaveValue("");
 
-  //   const searchButton = screen.getByRole("button", { name: /search/i });
-  //   await user.click(searchButton);
+    const searchButton = screen.getByRole("button", { name: /search/i });
+    await user.click(searchButton);
 
-  //   expect(dispatch).toHaveBeenCalled();
-  //   expect(mockedSaveString).toHaveBeenCalledWith({ inputValue: "pikachu" });
-  // });
+    await waitFor(() => {
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
 
-  // test("should render an input and a button", () => {
-  //   renderSearchInput();
+  test("should call dispatch with inputValue when there is input value", async () => {
+    const dispatch = jest.fn();
+    mockedDispatch.mockReturnValue(dispatch);
 
-  //   const inputElement = screen.getByRole("textbox");
-  //   const button = screen.getByRole("button", { name: /search/i });
+    render(<SearchInput />);
 
-  //   expect(inputElement).toBeInTheDocument();
-  //   expect(button).toBeInTheDocument();
-  //   expect(button).toBeEnabled();
-  // });
+    const inputElement: HTMLInputElement = screen.getByRole("textbox");
+    await user.type(inputElement, "pikachu");
 
-  // test("should save entered value to local storage on button click", async () => {
-  //   renderSearchInput();
+    const searchButton = screen.getByRole("button", { name: /search/i });
+    await user.click(searchButton);
 
-  //   const inputElement: HTMLInputElement = screen.getByRole("textbox");
-  //   await user.type(inputElement, "pikachu");
+    expect(dispatch).toHaveBeenCalled();
+    expect(mockedSaveString).toHaveBeenCalledWith({ inputValue: "pikachu" });
+  });
 
-  //   const searchButton = screen.getByRole("button", { name: /search/i });
-  //   await user.click(searchButton);
+  test("should render an input and a button", () => {
+    render(<SearchInput />);
 
-  //   expect(mockSetItem).toHaveBeenCalledTimes(1);
-  //   expect(mockSetItem).toHaveBeenCalledWith("searchString", "pikachu");
-  // });
+    const inputElement = screen.getByRole("textbox");
+    const button = screen.getByRole("button", { name: /search/i });
 
-  // test("should get value from the local storage upon mounting", async () => {
-  //   localStorage.setItem("searchString", "pikachu");
+    expect(inputElement).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(button).toBeEnabled();
+  });
 
-  //   renderSearchInput();
+  test("should save entered value to local storage on button click", async () => {
+    render(<SearchInput />);
 
-  //   expect(mockGetItem).toHaveBeenCalled();
-  //   expect(mockGetItem).toHaveBeenCalledWith("searchString");
+    //
+    const inputElement: HTMLInputElement = screen.getByRole("textbox");
+    await user.type(inputElement, "pikachu");
 
-  //   const inputElement: HTMLInputElement = screen.getByRole("textbox");
+    const searchButton = screen.getByRole("button", { name: /search/i });
+    await user.click(searchButton);
 
-  //   expect(inputElement).toBeInTheDocument();
-  //   expect(inputElement.value).toBe("pikachu");
-  // });
+    expect(mockSetItem).toHaveBeenCalledTimes(1);
+    expect(mockSetItem).toHaveBeenCalledWith("searchString", "pikachu");
+  });
 
-  // test("should render empty input with placeholder if searchString is empty", async () => {
-  //   localStorage.setItem("searchString", "");
+  test("should get value from the local storage upon mounting", async () => {
+    localStorage.setItem("searchString", "pikachu");
 
-  //   renderSearchInput();
+    render(<SearchInput />);
 
-  //   const inputElement: HTMLInputElement = screen.getByRole("textbox");
+    expect(mockGetItem).toHaveBeenCalled();
+    expect(mockGetItem).toHaveBeenCalledWith("searchString");
 
-  //   expect(inputElement).toBeInTheDocument();
-  //   expect(inputElement.value).toBe("");
-  //   expect(inputElement.placeholder).toBe("Search Pokemon");
-  // });
+    const inputElement: HTMLInputElement = screen.getByRole("textbox");
+
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement.value).toBe("pikachu");
+  });
+
+  test("should render empty input with placeholder if searchString is empty", async () => {
+    localStorage.setItem("searchString", "");
+
+    render(<SearchInput />);
+
+    const inputElement: HTMLInputElement = screen.getByRole("textbox");
+
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement.value).toBe("");
+    expect(inputElement.placeholder).toBe("Search Pokemon");
+  });
 });
