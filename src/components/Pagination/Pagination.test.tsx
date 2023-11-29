bc8afacc195191f62b4803288c0fe9a393cce1ed
Here East <nastiapiven@gmail.com>
2023-11-30 00:00:01 +0100

    feat: update tests for Pagination

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -1,79 +1,56 @@
 import "@testing-library/jest-dom";
 
-// import * as reduxHooks from "react-redux";
-// import user from "@testing-library/user-event";
-// import { render, screen } from "@testing-library/react";
-// import { MemoryRouter } from "react-router-dom";
-// import { Pagination } from "./index";
+import * as reduxHooks from "react-redux";
+import user from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { useRouter } from "next/router";
+import { Pagination } from "./index";
 
-// jest.mock("react-redux");
+jest.mock("react-redux");
 
-// jest.spyOn(reduxHooks, "useDispatch").mockReturnValue(jest.fn());
-// jest.spyOn(reduxHooks, "useSelector").mockReturnValue({ isLoading: false });
+jest.spyOn(reduxHooks, "useDispatch").mockReturnValue(jest.fn());
+jest.spyOn(reduxHooks, "useSelector").mockReturnValue({ isLoading: false });
 
-// type Param = { page: string };
-// type UseSearchParamsReturn = [URLSearchParams, (params: Param) => void];
+jest.mock("next/router", () => {
+  const router = {
+    push: jest.fn(),
+    query: { limit: "30", page: "5" },
+  };
+  return {
+    useRouter: jest.fn().mockReturnValue(router),
+  };
+});
 
-// const mockSetSearchParams = jest.spyOn(URLSearchParams.prototype, "set");
-// const mockSearchParams = new URLSearchParams("page=5");
-
-// jest.mock("react-router-dom", () => ({
-//   ...jest.requireActual("react-router-dom"),
-//   useSearchParams: (): UseSearchParamsReturn => [
-//     mockSearchParams,
-//     (params: Param): void => {
-//       mockSearchParams.set("page", params.page);
-//     },
-//   ],
-// }));
-
-// jest.mock("../../utils/getMaxPage", () => ({
-//   getMaxPage: jest.fn(async () => "10"),
-// }));
-
-// function renderPagination(): void {
-//   render(
-//     <MemoryRouter>
-//       <Pagination />
-//     </MemoryRouter>,
-//   );
-// }
+jest.mock("../../utils/getMaxPage", () => ({
+  getMaxPage: jest.fn(async () => "10"),
+}));
 
 describe("Pagination component", () => {
-  // beforeEach(() => {
-  //   mockSearchParams.set("page", "5");
-  // });
-
-  test("should update page parameters in URL on click on buttons", async () => {
-    expect(true).toBe(true);
-    // renderPagination();
-
-    // const buttonNext = screen.getByRole("button", { name: /next/i });
-    // expect(buttonNext).toBeInTheDocument();
-
-    // expect(mockSearchParams.get("page")).toBe("5");
-
-    // await user.click(buttonNext);
-
-    // expect(mockSetSearchParams).toHaveBeenCalled();
-    // expect(mockSetSearchParams).toHaveBeenCalledWith("page", "6");
-
-    // expect(mockSearchParams.get("page")).toBe("6");
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  // test("should update page parameters in URL on click on button Prev", async () => {
-  //   renderPagination();
+  test("should update page parameters in URL on click on buttons", async () => {
+    render(<Pagination />);
 
-  //   const buttonPrev = screen.getByRole("button", { name: /prev/i });
-  //   expect(buttonPrev).toBeInTheDocument();
+    const buttonNext = screen.getByRole("button", { name: /next/i });
+    expect(buttonNext).toBeInTheDocument();
 
-  //   expect(mockSearchParams.get("page")).toBe("5");
+    await user.click(buttonNext);
 
-  //   await user.click(buttonPrev);
+    expect(useRouter().push).toHaveBeenCalledTimes(1);
+    expect(useRouter().push).toHaveBeenCalledWith({ pathname: "/", query: { limit: "30", page: 6 } });
+  });
 
-  //   expect(mockSetSearchParams).toHaveBeenCalled();
-  //   expect(mockSetSearchParams).toHaveBeenCalledWith("page", "4");
+  test("should update page parameters in URL on click on button Prev", async () => {
+    render(<Pagination />);
 
-  //   expect(mockSearchParams.get("page")).toBe("4");
-  // });
+    const buttonPrev = screen.getByRole("button", { name: /prev/i });
+    expect(buttonPrev).toBeInTheDocument();
+
+    await user.click(buttonPrev);
+
+    expect(useRouter().push).toHaveBeenCalledTimes(1);
+    expect(useRouter().push).toHaveBeenCalledWith({ pathname: "/", query: { limit: "30", page: 4 } });
+  });
 });
