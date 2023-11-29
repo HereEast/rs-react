856d35ffd2c08bc8a0d389c429a94cfae840aba1
Here East <nastiapiven@gmail.com>
2023-11-29 21:37:22 +0100

    feat: update tests for NotFound

--- src/components/pages/NotFound/NotFound.test.tsx
+++ src/components/pages/NotFound/NotFound.test.tsx
@@ -1,48 +1,60 @@
 import "@testing-library/jest-dom";
 
-// import user from "@testing-library/user-event";
-// import { render, screen } from "@testing-library/react";
-// import { MemoryRouter, Routes, Route } from "react-router-dom";
-// import { ReactElement } from "react";
-// import { NotFound } from "./index";
+import user from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { ReactElement } from "react";
+import { NotFound } from "./index";
+import { useRouter } from "next/router";
+
+jest.mock("next/router", () => {
+  const router = {
+    push: jest.fn(),
+    query: { limit: "30", page: "1" },
+  };
+  return {
+    useRouter: jest.fn().mockReturnValue(router),
+  };
+});
 
 describe("NotFound component", () => {
   test("should display NotFound page for invalid route", () => {
-    expect(true).toBe(true);
-    // const HomePage = (): ReactElement => <div>Home page</div>;
+    const HomePage = (): ReactElement => <div>Home page</div>;
 
-    // render(
-    //   <MemoryRouter initialEntries={["/not-found"]}>
-    //     <Routes>
-    //       <Route path="/" element={<HomePage />} />
-    //       <Route path="*" element={<NotFound />} />
-    //     </Routes>
-    //   </MemoryRouter>,
-    // );
+    render(
+      <MemoryRouter initialEntries={["/not-found"]}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </MemoryRouter>,
+    );
 
-    // const message = screen.getByText(/Page not found/i);
-    // const button = screen.getByRole("button", { name: /Back to main/i });
+    const message = screen.getByText(/Page not found/i);
+    const button = screen.getByRole("button", { name: /Back to main/i });
 
-    // expect(message).toBeInTheDocument();
-    // expect(button).toBeInTheDocument();
+    expect(message).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
   });
 
-  // test("should navigate to Home page on click on button", async () => {
-  //   const HomePage = (): ReactElement => <div>Home page</div>;
+  test("should navigate to Home page on click on button", async () => {
+    const HomePage = (): ReactElement => <div>Home page</div>;
 
-  //   render(
-  //     <MemoryRouter initialEntries={["/not-found"]}>
-  //       <Routes>
-  //         <Route path="/" element={<HomePage />} />
-  //         <Route path="*" element={<NotFound />} />
-  //       </Routes>
-  //     </MemoryRouter>,
-  //   );
+    render(
+      <MemoryRouter initialEntries={["/not-found"]}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </MemoryRouter>,
+    );
 
-  //   const button = screen.getByRole("button", { name: /Back to main/i });
-  //   expect(button).toBeInTheDocument();
+    const button = screen.getByRole("button", { name: /Back to main/i });
+    expect(button).toBeInTheDocument();
 
-  //   await user.click(button);
-  //   expect(screen.getByText("Home page")).toBeInTheDocument();
-  // });
+    await user.click(button);
+
+    expect(useRouter().push).toHaveBeenCalledTimes(1);
+    expect(useRouter().push).toHaveBeenCalledWith({ pathname: "/", query: { limit: "30", page: "1" } });
+  });
 });
