3bb93c27ee997fe92f5476ab0055789ce70bbc9f
Here East <nastiapiven@gmail.com>
2023-11-29 19:30:01 +0100

    feat: attempt to implement SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,14 @@ import { AppContextProvider } from "../src/context";
 import { Provider } from "react-redux";
 import { store } from "../src/store/store";
 
+// import { wrapper } from "../src/store/store";
+
 import "../src/styles/global.scss";
 
 function App({ Component, pageProps }: AppProps): ReactElement {
+  // const { store, props } = wrapper.useWrappedStore(rest);
+  // const { pageProps } = props;
+
   return (
     <AppContextProvider>
       <Provider store={store}>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,51 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import searchStringReducer from "./search/slice";
-import limitReducer from "./limit/slice";
 import { pokemonReducer } from "./pokemon/slice";
 import { pokemonDetailsReducer } from "./pokemonDetails/slice";
+
+import searchStringReducer from "./search/slice";
+import limitReducer from "./limit/slice";
+
+// import { Store, ThunkDispatch, AnyAction } from "@reduxjs/toolkit";
+// import { createWrapper, HYDRATE } from "next-redux-wrapper";
+
+// const rootReducer = {
+//   searchString: searchStringReducer,
+//   limit: limitReducer,
+//   pokemon: pokemonReducer,
+//   pokemonDetails: pokemonDetailsReducer,
+// };
+
+// const reducer = (state: any, action: any) => {
+//   if (action.type === HYDRATE) {
+//     return {
+//       ...state,
+//       ...action.payload,
+//     };
+//   }
+//   return rootReducer;
+// };
+
+// const store = (): Store =>
+//   configureStore({
+//     reducer,
+//     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+//   });
+
+// export const storeWrapper = createWrapper(store);
+
+// export type AppStore = ReturnType<typeof store>;
+// export type RootState = ReturnType<AppStore["getState"]>;
+// export type AppDispatch = AppStore["dispatch"];
+
+// Changed, added 2 lines, commented 1
+// export type AppThunkDispatch = ThunkDispatch<RootState, void, AnyAction>;
+// export const useAppDispatch: () => AppThunkDispatch = useDispatch;
+
+// export const useAppDispatch: () => AppDispatch = useDispatch;
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// export const wrapper = createWrapper<AppStore>(store, { debug: true });
 
 export const store = configureStore({
   reducer: {
