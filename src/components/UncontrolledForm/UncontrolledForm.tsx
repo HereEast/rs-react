f635cf799ea3a4934825196891992d451040330f
Here East <nastiapiven@gmail.com>
2023-12-01 22:17:06 +0100

    feat: render and validate UncontrolledForm

--- src/components/UncontrolledForm/UncontrolledForm.tsx
+++ src/components/UncontrolledForm/UncontrolledForm.tsx
@@ -1,96 +1,58 @@
 import { FormEvent, ReactElement, useRef, useState } from "react";
 import { Button } from "../Button";
 import { ErrorMessage } from "../ErrorMessage";
-// import { Input } from "../../components/Input";
-import { COUNTRIES } from "../../utils/constants";
+import { Input } from "../Input";
+import { validationSchema, initErrors, COUNTRIES } from "../../utils";
 
 import * as Yup from "yup";
 
 import classnames from "classnames";
 import styles from "./uncontrolledForm.module.scss";
 
-// validate on submit
 // store data in redux store >>> show on Home after successful submit
-
 // autocomplete control to select country (all countries should be stored in the Redux store)
 
-const validationSchema = Yup.object({
-  name: Yup.string()
-    .required("Name is required.")
-    .matches(/^[A-Z].*$/, "Name should start with an uppercase letter."),
-  age: Yup.string()
-    .required("Age is required.")
-    .matches(/^[1-9]\d*$/, "Age should be a positive number."),
-  country: Yup.string()
-    .required("Country is required.")
-    .test(
-      "isValidCountry",
-      "Please, select a country from the list.",
-      (value) => {
-        return !value || COUNTRIES.includes(value);
-      },
-    ),
-  // .oneOf(COUNTRIES, "Please, select a country from the list."),
-  email: Yup.string()
-    .required("Email is required.")
-    .email("Please, enter a valid email."),
-});
-
 function UncontrolledForm(): ReactElement {
-  const nameInputRef = useRef<HTMLInputElement>(null);
-  const ageInputRef = useRef<HTMLInputElement>(null);
-  const emailInputRef = useRef<HTMLInputElement>(null);
-  const countryInputRef = useRef<HTMLInputElement>(null);
+  const inputRef = {
+    name: useRef<HTMLInputElement>(null),
+    age: useRef<HTMLInputElement>(null),
+    email: useRef<HTMLInputElement>(null),
+    country: useRef<HTMLInputElement>(null),
+    checkbox: useRef<HTMLInputElement>(null),
+    password: useRef<HTMLInputElement>(null),
+    passwordRepeat: useRef<HTMLInputElement>(null),
+    file: useRef<HTMLInputElement>(null),
+  };
 
-  const [nameError, setNameError] = useState<string[]>([]);
-  const [ageError, setAgeError] = useState<string[]>([]);
-  const [countryError, setCountryError] = useState<string[]>([]);
-  const [emailError, setEmailError] = useState<string[]>([]);
+  const [errors, setErrors] = useState(initErrors);
 
   async function handleSubmit(e: FormEvent): Promise<void> {
     e.preventDefault();
 
+    const formData = {
+      name: inputRef.name.current?.value.trim(),
+      age: inputRef.age.current?.value.trim(),
+      email: inputRef.email.current?.value.trim(),
+      country: inputRef.country.current?.value.trim(),
+      checkbox: inputRef.checkbox.current?.checked,
+      password: inputRef.password.current?.value.trim(),
+      passwordRepeat: inputRef.passwordRepeat.current?.value.trim(),
+      file: inputRef.file.current?.files?.[0],
+    };
+
     try {
-      await validationSchema.validate(
-        {
-          name: nameInputRef.current?.value.trim(),
-          age: ageInputRef.current?.value.trim(),
-          email: emailInputRef.current?.value.trim(),
-          country: countryInputRef.current?.value.trim(),
-        },
-        { abortEarly: false },
-      );
+      await validationSchema.validate(formData, { abortEarly: false });
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
-        const currentNameErrors: string[] = [];
-        const currentAgeErrors: string[] = [];
-        const currentEmailErrors: string[] = [];
-        const currentCountryErrors: string[] = [];
+        const currentErrors = JSON.parse(JSON.stringify(initErrors));
 
         err.inner.forEach((e) => {
-          // console.log(e.path, e.message);
-
-          if (e.path && e.path === "name") {
-            currentNameErrors.push(e.message);
-          }
-
-          if (e.path && e.path === "age") {
-            currentAgeErrors.push(e.message);
-          }
-
-          if (e.path && e.path === "country") {
-            currentCountryErrors.push(e.message);
-          }
-
-          if (e.path && e.path === "email") {
-            currentEmailErrors.push(e.message);
+          if (e.path && currentErrors[e.path]) {
+            currentErrors[e.path].push(e.message);
           }
         });
 
-        setNameError(currentNameErrors);
-        setAgeError(currentAgeErrors);
-        setCountryError(currentCountryErrors);
-        setEmailError(currentEmailErrors);
+        setErrors(currentErrors);
       }
     }
   }
@@ -102,18 +64,19 @@ function UncontrolledForm(): ReactElement {
       <form className={styles.form} noValidate>
         {/* Name */}
         <div className={styles.container__name}>
-          {/* <Input type="text" label="Name" name="name" ref={nameInputRef} /> */}
-          <label htmlFor="name" className={styles.label}>
+          <Input type="text" name="name" label="Name" ref={inputRef.name} />
+          {/* <label htmlFor="name" className={styles.label}>
             Name
             <input
               type="text"
               name="name"
               id="name"
               className={styles.input}
-              ref={nameInputRef}
+              ref={inputRef.name}
             />
-          </label>
-          {nameError.length > 0 && <ErrorMessage message={nameError[0]} />}
+          </label> */}
+
+          {errors.name.length > 0 && <ErrorMessage message={errors.name[0]} />}
         </div>
 
         {/* Age */}
@@ -125,10 +88,11 @@ function UncontrolledForm(): ReactElement {
               name="age"
               id="age"
               className={styles.input}
-              ref={ageInputRef}
+              ref={inputRef.age}
             />
           </label>
-          {ageError.length > 0 && <ErrorMessage message={ageError[0]} />}
+
+          {errors.age.length > 0 && <ErrorMessage message={errors.age[0]} />}
         </div>
 
         {/* Country */}
@@ -141,20 +105,20 @@ function UncontrolledForm(): ReactElement {
               id="country"
               list="countries"
               className={styles.input}
-              ref={countryInputRef}
+              ref={inputRef.country}
             />
           </label>
 
           <datalist id="countries">
-            {COUNTRIES.map((option) => (
-              <option key={option} value={option}>
+            {COUNTRIES.map((option, idx) => (
+              <option key={idx} value={option}>
                 {option}
               </option>
             ))}
           </datalist>
 
-          {countryError.length > 0 && (
-            <ErrorMessage message={countryError[0]} />
+          {errors.country.length > 0 && (
+            <ErrorMessage message={errors.country[0]} />
           )}
         </div>
 
@@ -167,33 +131,48 @@ function UncontrolledForm(): ReactElement {
               name="email"
               id="email"
               className={styles.input}
-              ref={emailInputRef}
+              ref={inputRef.email}
             />
           </label>
-          {emailError.length > 0 && <ErrorMessage message={emailError[0]} />}
+
+          {errors.email.length > 0 && (
+            <ErrorMessage message={errors.email[0]} />
+          )}
         </div>
 
         {/* Password */}
         <div className={styles.container__password}>
-          <label htmlFor="password" className={styles.label}>
-            Password
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className={styles.input}
-            />
-          </label>
+          <div className={styles.fields}>
+            <label htmlFor="password" className={styles.label}>
+              Password
+              <input
+                type="text"
+                name="password"
+                id="password"
+                className={styles.input}
+                ref={inputRef.password}
+              />
+            </label>
 
-          <label htmlFor="password-repeat" className={styles.label}>
-            Repeat Password
-            <input
-              type="password"
-              name="password-repeat"
-              id="password-repeat"
-              className={styles.input}
-            />
-          </label>
+            <label htmlFor="passwordRepeat" className={styles.label}>
+              Repeat Password
+              <input
+                type="text"
+                name="passwordRepeat"
+                id="passwordRepeat"
+                className={styles.input}
+                ref={inputRef.passwordRepeat}
+              />
+            </label>
+          </div>
+
+          {errors.password.length > 0 && (
+            <ErrorMessage message={errors.password[0]} />
+          )}
+
+          {!errors.password.length && errors.passwordRepeat.length > 0 && (
+            <ErrorMessage message={errors.passwordRepeat[0]} />
+          )}
         </div>
 
         {/* Gender */}
@@ -222,28 +201,38 @@ function UncontrolledForm(): ReactElement {
 
         {/* Image */}
         <div className={styles.container__file}>
-          <label htmlFor="image-upload">
-            <input
-              type="file"
-              name="image-upload"
-              id="image-upload"
-              className={classnames(styles.input, styles.input__file)}
-              accept="image/png, image/jpeg"
-            />
-          </label>
+          <div className={styles.control}>
+            <label htmlFor="file">
+              <input
+                type="file"
+                name="file"
+                id="file"
+                className={classnames(styles.input, styles.input__file)}
+                accept="image/png, image/jpeg"
+                ref={inputRef.file}
+              />
+            </label>
+          </div>
+
+          {errors.file.length > 0 && <ErrorMessage message={errors.file[0]} />}
         </div>
 
         {/* Checkbox */}
         <div className={styles.container__checkbox}>
-          <label htmlFor="check" className={styles.label}>
+          <label htmlFor="checkbox" className={styles.label}>
             Agree with Terms & Conditions.
             <input
               type="checkbox"
-              name="check"
-              id="check"
+              name="checkbox"
+              id="checkbox"
               className={classnames(styles.input, styles.input__checkbox)}
+              ref={inputRef.checkbox}
             />
           </label>
+
+          {errors.checkbox.length > 0 && (
+            <ErrorMessage message={errors.checkbox[0]} />
+          )}
         </div>
 
         <Button
