ca7d3038c6ff6bc1aaabee910b31acd15a9dbe98
Here East <nastiapiven@gmail.com>
2023-12-02 01:42:13 +0100

    feat: add formData slice to store

---
+++ src/store/form/index.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { IFormData } from "../../types";
+
+interface IInitialData {
+  data: IFormData;
+}
+
+export const initialData: IInitialData = {
+  data: {
+    name: "",
+    age: "",
+    email: "",
+    country: "",
+    password: "",
+    file: {
+      fileName: "",
+      fileSize: 1234,
+      fileBase: "",
+    },
+    gender: "",
+  },
+};
+
+export const formDataSlice = createSlice({
+  name: "formData",
+  initialState: initialData,
+  reducers: {
+    saveFormData(state, action) {
+      state.data = action.payload;
+    },
+    saveFileBase(state, action) {
+      state.data.file.fileBase = action.payload;
+    },
+  },
+});
+
+export const { saveFormData, saveFileBase } = formDataSlice.actions;
+export default formDataSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import countriesReducer from "./countries";
+import formDataReducer from "./form";
 
 export const store = configureStore({
   reducer: {
     countries: countriesReducer,
+    formData: formDataReducer,
   },
 });
 
